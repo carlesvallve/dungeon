999a667751bcb626da84e69a2c7a66036f3f2d7c
Carles Vallve <snaptothegrid@gmail.com>
2014-10-18 17:14:50 +0900

    Implemented torch effect

---
+++ Assets/Scripts/lights/Torch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+	private Vector3 center;
+	private float d = 0.25f;
+
+	void Start () {
+		center = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		int prob = Random.Range(1, 100);
+		if (prob <= 25) {
+			transform.localPosition = center + new Vector3(Random.Range(-d, d), Random.Range(-d, d), Random.Range(-d, d));
+		}
+		
+	}
+}
