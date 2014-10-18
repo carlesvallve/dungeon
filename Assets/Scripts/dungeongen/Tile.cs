143b4b82d745c2d7c5c693de8d654a95d1062ff7
Carles Vallve <snaptothegrid@gmail.com>
2014-10-18 13:19:26 +0900

    Implemented prime dungeon generator

---
+++ Assets/Scripts/dungeongen/Tile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+//[System.Serializable]
+
+public class Tile {
+
+	// to avoid unity 4.5 "exceded depth" warnings
+	[System.NonSerialized]
+
+	// Tile Types
+	public const int TILE_EMPTY = 0;
+	public const int TILE_ROOM = 1;
+	public const int TILE_WALL = 2;
+	public const int TILE_CORRIDOR = 3;
+	
+	// Tile ID
+	public int id;
+	
+	public Tile ( int _id ) {
+		this.id = _id;
+	}
+}
